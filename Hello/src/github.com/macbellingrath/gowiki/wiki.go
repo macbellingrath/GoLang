baea68f5cb6411cebded506a240860a608bbb1ff
macbellingrath <macbellingrath@gmail.com>
2015-12-26 09:42:23 -0500

    Add GoWiki

---
+++ Hello/src/github.com/macbellingrath/gowiki/wiki.go
@@ -0,0 +1,39 @@
+package main 
+
+import (
+	"fmt"
+	"net/http"
+	"io/ioutil"
+)
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+
+}
+
+type Page struct {
+	Title string
+	Body []byte
+}
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+} 
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+
+
+	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample page.")}
+	p1.save()
+	p2, _ := loadPage("TestPage")
+	fmt.Println(string(p2.Body))
+}
